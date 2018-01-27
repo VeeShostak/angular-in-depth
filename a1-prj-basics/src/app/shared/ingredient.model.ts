d6c004e7af6da7f38e42672c85bc685ef9b24faa
VeeShostak <VeeShostak@users.noreply.github.com>
2018-01-27 11:40:34 -0500

    create ingredient model that will be shared

---
+++ a1-prj-basics/src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+// using accessor public: behind the scenes it will create the properties and assign them to the passed args
+export class Ingredient {
+    constructor(public name: string, public amount: number) {}
+  }
+  
