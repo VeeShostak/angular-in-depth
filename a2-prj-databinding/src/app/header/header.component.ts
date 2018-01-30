69684a73734b9098cc63e1cc175e930f0cd2690c
VeeShostak <VeeShostak@users.noreply.github.com>
2018-01-29 23:31:58 -0500

    add comment

--- a2-prj-databinding/src/app/header/header.component.ts
+++ a2-prj-databinding/src/app/header/header.component.ts
@@ -5,6 +5,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  // Output (notify) to anybody that is observing (listening) (pass data to the outside world)
+  // EventEmitter an object (notify object), that listens for something to happen and emits 
+  // an event when triggered (observer pattern)
   @Output() featureSelected = new EventEmitter<string>();
 
   onSelect(feature: string) {
