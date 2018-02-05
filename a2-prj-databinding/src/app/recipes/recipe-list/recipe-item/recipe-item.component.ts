804e6780eccf6c08820796a9ec5e58cd58758ed3
VeeShostak <VeeShostak@users.noreply.github.com>
2018-02-04 22:36:51 -0600

    add comments

--- a2-prj-databinding/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ a2-prj-databinding/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,7 +11,7 @@ export class RecipeItemComponent implements OnInit {
   // take recipe as an input
   @Input() recipe: Recipe;
 
-  // listen to this event from the outside
+  // listen to this event from the outside. someone from outside can listen to this as its state changes
   @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
